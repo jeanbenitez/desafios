62e95464a648203029fc8c23da6ffed993dd02a4
Jean Benitez <jkbdiaz@gmail.com>
2018-11-03 21:12:23 -0500

    array sum

---
+++ src/keysToArray.test.js
@@ -0,0 +1,30 @@
+import { keysToArray } from './keysToArray';
+
+/*
+ * Example of the assertions you can use for your tests:
+ *
+ * - Testing equality between the function's output and a constant:
+ * expect(keysToArray()).toEqual(123);
+ *
+ * - Testing that the function returns null:
+ * expect(keysToArray()).toBeNull();
+ *
+ * - Testing that the function returns a falsy value (eg. false, 0, "")
+ * expect(keysToArray()).toBeFalsy();
+ *
+ * - Testing that the function returns a truthy value (eg. true, 1, "abc")
+ * expect(keysToArray()).toBeTruthy();
+ *
+ * - Testing that the function throws
+ * expect(() => { keysToArray(); }).toThrow();
+ */
+
+describe('keysToArray', () => {
+  it('keysToArray debe hacer esto...', () => {
+    // TODO Your own assertion here
+  });
+
+  it('keysToArray debe hacer esto otro...', () => {
+    // TODO Your own assertion here
+  });
+});
