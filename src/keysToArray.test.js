6eeab9317f03b6d4a8e8dfc165c05ccbd6607ed4
Jean Benitez <jkbdiaz@gmail.com>
2018-11-04 20:28:35 -0500

    fix tests

--- src/keysToArray.test.js
+++ src/keysToArray.test.js
@@ -25,8 +25,10 @@ describe('keysToArray', () => {
   });
 
   it('keysToArray debe arrojar errores con malos argumentos...', () => {
+    expect(() => { keysToArray({}); }).toThrow(); // object without keys
     expect(() => { keysToArray(); }).toThrow();
     expect(() => { keysToArray([90]); }).toThrow();
     expect(() => { keysToArray("what"); }).toThrow();
+    expect(() => { keysToArray(true); }).toThrow();
   });
 });
