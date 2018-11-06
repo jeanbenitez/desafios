248f3f46a013dade0c6702d8d7fff6980417215e
Jean Benitez <jeanbenitez@users.noreply.github.com>
2018-11-06 15:19:27 -0500

    Update odiousNumbers.test.js

--- src/odiousNumbers.test.js
+++ src/odiousNumbers.test.js
@@ -1,11 +1,11 @@
 import {
-  odiousNumbers
-} from './odiousNumbers';
+  numerosOdiosos
+} from './numerosOdiosos';
 
-describe('odiousNumbers', () => {
-  it('returns odious numbers for specified range', () => {
-    expect(odiousNumbers(0, 50)).toEqual([1, 2, 4, 7, 8, 11, 13, 14, 16, 19, 21, 22, 25, 26, 28, 31, 32, 35, 37, 38, 41, 42, 44, 47, 49, 50]);
-    expect(odiousNumbers(30, 80)).toEqual([31, 32, 35, 37, 38, 41, 42, 44, 47, 49, 50, 52, 55, 56, 59, 61, 62, 64, 67, 69, 70, 73, 74, 76, 79]);
-    expect(odiousNumbers(1, 2)).toEqual([1, 2]);
+describe('numerosOdiosos', () => {
+  it('retorna numeros odiosos de un rango dado', () => {
+    expect(numerosOdiosos(0, 50)).toEqual([1, 2, 4, 7, 8, 11, 13, 14, 16, 19, 21, 22, 25, 26, 28, 31, 32, 35, 37, 38, 41, 42, 44, 47, 49, 50]);
+    expect(numerosOdiosos(30, 80)).toEqual([31, 32, 35, 37, 38, 41, 42, 44, 47, 49, 50, 52, 55, 56, 59, 61, 62, 64, 67, 69, 70, 73, 74, 76, 79]);
+    expect(numerosOdiosos(1, 2)).toEqual([1, 2]);
   });
 });
