d61cc19d44704fc1993f77903ed47a1080c67ea8
krrskl97 <carrascalruben@gmail.com>
2018-11-07 15:43:31 -0500

    subiendo la solución de capitalize.js y agregando desafío paresMatriz

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,3 +1,14 @@
-export const capitalize = () => {
-  // SE IMPLEMENTARA EN OTRO PR
+export const capitalize = (data) => {
+  let dat = data.toLowerCase();
+
+  if (typeof data === 'number')
+    throw "No se aceptan números";
+  if (typeof data === 'object')
+    throw "No se aceptan objectos";
+  if (typeof data === 'array')
+    throw "No se aceptan arreglos";
+  if (data.length <= 0)
+    throw "Cadena incorrecta";
+
+  return dat.replace(/^\w/, (c) => c.toUpperCase());
 };
---
+++ src/paresMatriz.js
@@ -0,0 +1,3 @@
+export const paresMatriz = () => {
+  // Implementar AQUI
+};
