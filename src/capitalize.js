7108aa837b4660392a876d9af895546786285893
krrskl97 <carrascalruben@gmail.com>
2018-11-07 17:16:00 -0500

    corrigiendo detalles de lint y actualizando el código

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,14 +1,8 @@
 export const capitalize = (data) => {
   let dat = data.toLowerCase();
-
-  if (typeof data === 'number')
-    throw "No se aceptan números";
-  if (typeof data === 'object')
-    throw "No se aceptan objectos";
-  if (typeof data === 'array')
-    throw "No se aceptan arreglos";
-  if (data.length <= 0)
-    throw "Cadena incorrecta";
-
-  return dat.replace(/^\w/, (c) => c.toUpperCase());
+  if (data.constructor === String) {
+    if (data.length <= 0)
+      throw "Cadena vacía";
+    return dat.replace(/^\w/, (c) => c.toUpperCase());
+  } else throw `Argumento inválido, no se reciben ${data.constructor}.`;
 };
